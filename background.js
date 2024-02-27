5a63b2a8ce592cc6a0eb3f6aa146d00803b745ea
TaylorTayTyT <taylornguyen904@gmail.com>
2024-02-27 18:24:01 -0500

    saving to storage

--- background.js
+++ background.js
@@ -5,9 +5,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   //base url is: https://johnshopkins.employment.ngwebsolutions.com/
   //Tsx_StuManageTimesheet.aspx?TsId=894518
   if (tab.url.includes("add=true#entries") && tab.url.includes("fuckTimesheet=True")) {
-    const info = {
-      
-    } 
     chrome.tabs.sendMessage(tabId, "SET_TIMES");
   }
   chrome.tabs.sendMessage(tabId, "tabID");
--- popup.js
+++ popup.js
@@ -1,20 +1,68 @@
 // adding a new bookmark row to the popup
 
 //this executes once you click the popup
-chrome.tabs.query({active:true, currentWindow: true}, (tabs) => {
-    console.log('start up');
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
-    function populate(){
+    function generate_display_time(weekday){
+
+    }
+
+    function update_time_display(){
+        const time_display = document.getElementById("saved_times");
+        const curr_day = document.getElementsByClassName("selected")[0].id;
+        chrome.storage.local.get(null, function(items){
+            const allKeys = Object.keys(items);
+            let app_keys = [];
+            for (let i = 0; i < allKeys.length; i++) {
+                if(allKeys[i].includes(curr_day)) {
+                    app_keys.push(allKeys[i]);
+                }
+            }
+            app_keys.forEach(element=>{
+                const saved_times = document.getElementById("saved_times");
+                const new_elem = document.createElement("div");
+                new_elem.innerText = element.toString(); 
+                console.log(new_elem);
+                saved_times.appendChild(new_elem);
+            })
+        })
+    }
+    function save(){
+        const startTime = document.getElementById("clock_in").value;
+        const endTime = document.getElementById("clock_out").value;
+        const dayOfWeek = document.getElementsByClassName("selected")[0].id;
+        const storageID = dayOfWeek + " " + startTime;
+        const shift = {
+            "start": startTime, 
+            "end": endTime
+        }
+        chrome.storage.local.set({[storageID]: shift});
+        update_time_display(); 
+
+    }
+    function dayOfWeekSelected(event) {
+        const daysOfWeek = document.getElementsByClassName("day_of_week");
+        for (let i = 0; i < daysOfWeek.length; i++) {
+            daysOfWeek[i].classList.remove("selected");
+        }
+        event.target.classList.add('selected');
+    }
+
+    function populate() {
         console.log('will_populate');
         chrome.tabs.sendMessage(tabs[0].id, "EXECUTE");
     }
-    function get_schedule_info(){
-        chrome.storage.local.get(["1"], (result)=>{
-            console.log(result);
-        })
+    function initialize() {
+        document.getElementById('populate').onclick = populate;
+        document.getElementById("save_item").onclick = save;
+        const daysOfWeek = document.getElementsByClassName("day_of_week");
+        for (let i = 0; i < daysOfWeek.length; i++) {
+            daysOfWeek[i].addEventListener('click', (e) => dayOfWeekSelected(e));
+        }
     }
-    document.getElementById('populate').onclick = populate;
-    document.getElementById('see_schedule').onclick = get_schedule_info;
+
+    initialize(); 
+
     /** 
     document.getElementById('save_info_form').addEventListener("submit", (e)=>{
         e.preventDefault(); 
