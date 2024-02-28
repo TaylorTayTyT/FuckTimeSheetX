9d8b1491bc6fa6a555aa36ae955140c5e8ea59ea
TaylorTayTyT <taylornguyen904@gmail.com>
2024-02-28 10:15:28 -0500

    added some error handling

--- popup.js
+++ popup.js
@@ -1,43 +1,47 @@
 // adding a new bookmark row to the popup
-
 //this executes once you click the popup
+import {valid_time} from "./utiliy.js"
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-
-    function generate_display_time(weekday){
-
+    function clear_display(){
+        const saved_times = document.getElementById('saved_times');
+        saved_times.innerHTML = "";
     }
-
-    function update_time_display(){
-        const time_display = document.getElementById("saved_times");
+    function update_time_display() {
+        clear_display(); 
         const curr_day = document.getElementsByClassName("selected")[0].id;
-        chrome.storage.local.get(null, function(items){
+        chrome.storage.local.get(null, function (items) {
             const allKeys = Object.keys(items);
             let app_keys = [];
             for (let i = 0; i < allKeys.length; i++) {
-                if(allKeys[i].includes(curr_day)) {
+                if (allKeys[i].includes(curr_day)) {
                     app_keys.push(allKeys[i]);
                 }
             }
-            app_keys.forEach(element=>{
-                const saved_times = document.getElementById("saved_times");
-                const new_elem = document.createElement("div");
-                new_elem.innerText = element.toString(); 
-                console.log(new_elem);
-                saved_times.appendChild(new_elem);
+            app_keys.forEach(element => {
+                chrome.storage.local.get(element, (result) => {
+                    const saved_times = document.getElementById("saved_times");
+                    const new_elem = document.createElement("div");
+                    new_elem.innerText = element.toString() + " " + result[element]['end'];
+                    saved_times.appendChild(new_elem);
+                })
+
             })
         })
     }
-    function save(){
+    function save() {
         const startTime = document.getElementById("clock_in").value;
         const endTime = document.getElementById("clock_out").value;
+        if(!valid_time()){
+            return; 
+        }
         const dayOfWeek = document.getElementsByClassName("selected")[0].id;
         const storageID = dayOfWeek + " " + startTime;
         const shift = {
-            "start": startTime, 
+            "start": startTime,
             "end": endTime
         }
-        chrome.storage.local.set({[storageID]: shift});
-        update_time_display(); 
+        chrome.storage.local.set({ [storageID]: shift });
+        update_time_display();
 
     }
     function dayOfWeekSelected(event) {
@@ -46,8 +50,8 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             daysOfWeek[i].classList.remove("selected");
         }
         event.target.classList.add('selected');
+        update_time_display();
     }
-
     function populate() {
         console.log('will_populate');
         chrome.tabs.sendMessage(tabs[0].id, "EXECUTE");
@@ -55,31 +59,16 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     function initialize() {
         document.getElementById('populate').onclick = populate;
         document.getElementById("save_item").onclick = save;
+        document.getElementById("clear").onclick = ()=>{
+            chrome.storage.local.clear(); 
+            update_time_display(); 
+        } 
         const daysOfWeek = document.getElementsByClassName("day_of_week");
         for (let i = 0; i < daysOfWeek.length; i++) {
             daysOfWeek[i].addEventListener('click', (e) => dayOfWeekSelected(e));
         }
+        update_time_display();
     }
 
-    initialize(); 
-
-    /** 
-    document.getElementById('save_info_form').addEventListener("submit", (e)=>{
-        e.preventDefault(); 
-        
-        const dayOfWeek = document.getElementById("week").value;
-        const startTime = document.getElementById("start_time").value;
-        const endTime = document.getElementById("end_time").value;
-        const work_info = {
-            "day": dayOfWeek, 
-            "start": startTime,
-            "end": endTime
-        }
-        chrome.storage.local.set({"1": work_info}, ()=>{
-            chrome.storage.local.get(["1"], (result)=>{
-                console.log(result);
-            })
-        });
-        console.log(dayOfWeek);
-    })*/
+    initialize();
 })
---
+++ utility.js
@@ -0,0 +1,14 @@
+export function valid_time(startTime, endTime, allTimes){
+    //09:44
+    const startHour = startTime.substring(0, 2);
+    const endHour = endTime.substring(0, 2);
+    const startMin = startTime.substring(3, 5);
+    const endMin = endTime.substring(3, 5);
+    
+    if(endHour - startHour < 0) {
+        return false;
+    }
+    if(endHour == startHour && endTime - endMin < 0) {
+        return false; 
+    }
+}
