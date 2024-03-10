bc48751a3bf1fc24817471d9510835d981799245
TaylorTayTyT <taylornguyen904@gmail.com>
2024-03-10 12:03:09 -0400

    cleaned up the files a little bit

---
+++ display_utility.js
@@ -0,0 +1,32 @@
+export function clear_display() {
+    const curr_day = document.getElementsByClassName("selected")[0].id;
+    chrome.storage.local.set({ [curr_day]: { "status": "unset" } });
+    document.getElementById("saved_times").innerHTML = "";
+}
+export function display(time) {
+    const newElement = document.createElement('div');
+    newElement.classList.add("added_time");
+    const time_text = `start: ${time.start} \t end: ${time.end}`;
+    newElement.innerText = time_text;
+    document.getElementById("saved_times").appendChild(newElement);
+}
+export function update_time_display() {
+    document.getElementById("saved_times").innerHTML = "";
+    const curr_day = document.getElementsByClassName("selected")[0].id;
+    chrome.storage.local.get(curr_day, (results) => {
+        let time_keys = Object.keys(results[curr_day]);
+        time_keys.pop();
+        time_keys.forEach((element) => {
+            display(results[curr_day][element]);
+        })
+    })
+}
+
+export function dayOfWeekSelected(event) {
+    const daysOfWeek = document.getElementsByClassName("day_of_week");
+    for (let i = 0; i < daysOfWeek.length; i++) {
+        daysOfWeek[i].classList.remove("selected");
+    }
+    event.target.classList.add('selected');
+    update_time_display();
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 // adding a new bookmark row to the popup
 //this executes once you click the popup
-import valid_time, { this_week_dates } from './utility.js';
+import { clear_display, update_time_display, dayOfWeekSelected } from './display_utility.js';
+import valid_time from './time_utility.js';
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     function round_down(event) {
         const val_min = parseInt(event.target.value.substring(3, 5));
@@ -11,97 +12,69 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const final_time = event.target.value.substring(0, 3) + rounded_min;
         document.getElementById(event.target.id).value = final_time;
     }
-    function clear_display() {
-        const curr_day = document.getElementsByClassName("selected")[0].id;
-        chrome.storage.local.set({ [curr_day]: { "status": "unset" } });
-        document.getElementById("saved_times").innerHTML = "";
-    }
-    function display(time) {
-        const newElement = document.createElement('div');
-        newElement.classList.add("added_time");
-        const time_text = `start: ${time.start} \t end: ${time.end}`;
-        newElement.innerText = time_text;
-        document.getElementById("saved_times").appendChild(newElement);
-    }
-    function update_time_display() {
-        document.getElementById("saved_times").innerHTML = "";
-        const curr_day = document.getElementsByClassName("selected")[0].id;
-        chrome.storage.local.get(curr_day, (results) => {
-            let time_keys = Object.keys(results[curr_day]);
-            time_keys.pop();
-            time_keys.forEach((element) => {
-                display(results[curr_day][element]);
-            })
-        })
-    }
-    async function save() {
-        const startTime = document.getElementById("clock_in").value;
-        const endTime = document.getElementById("clock_out").value;
+    
+async function save() {
+    const startTime = document.getElementById("clock_in").value;
+    const endTime = document.getElementById("clock_out").value;
 
-        const new_times = valid_time(startTime, endTime)
-        if (!new_times) {
-            return;
-        }
-        const [new_start, new_end] = new_times;
-        const dayOfWeek = document.getElementsByClassName("selected")[0].id;
-        const shift = {
-            "start": new_start,
-            "end": new_end
-        }
-        chrome.storage.local.get(null, (data) => {
-            let data_mut = data[dayOfWeek];
-            let num_keys = Object.keys(data[dayOfWeek]).length;
-            data_mut[(num_keys + 1).toString()] = shift;
-            data_mut['status'] = 'set';
-            chrome.storage.local.set({ [dayOfWeek]: data_mut })
-            update_time_display();
-        })
+    const new_times = valid_time(startTime, endTime)
+    if (!new_times) {
+        return;
     }
-    function dayOfWeekSelected(event) {
-        const daysOfWeek = document.getElementsByClassName("day_of_week");
-        for (let i = 0; i < daysOfWeek.length; i++) {
-            daysOfWeek[i].classList.remove("selected");
-        }
-        event.target.classList.add('selected');
+    const [new_start, new_end] = new_times;
+    const dayOfWeek = document.getElementsByClassName("selected")[0].id;
+    const shift = {
+        "start": new_start,
+        "end": new_end
+    }
+    chrome.storage.local.get(null, (data) => {
+        let data_mut = data[dayOfWeek];
+        let num_keys = Object.keys(data[dayOfWeek]).length;
+        data_mut[(num_keys + 1).toString()] = shift;
+        data_mut['status'] = 'set';
+        chrome.storage.local.set({ [dayOfWeek]: data_mut })
         update_time_display();
-    }
-    async function populate() {
-        await populate_action();
-    }
-    async function populate_action() {
-        let message = {
-            action: "EXECUTE",
-        }
-        function set_stack(items) {
-            console.log(items);
-            try{
-                delete items['stack']['stack'];
-            } catch(e) {
-                console.log(e)
-            } finally {
-                chrome.storage.local.set({ "stack": items })
-                .then(() => {
-                    chrome.tabs.sendMessage(tabs[0].id, message);
-                })
-            }
-        }
+    })
+}
 
-        function remove_status(items) {
-            const item_keys = Object.keys(items)
-            item_keys.forEach(weekday=>{
-                if(weekday != "stack") {
-                    if(items[weekday].hasOwnProperty("status")) {
-                        delete items[weekday]["status"];
-                    }
-                }
+async function populate() {
+    await populate_action();
+}
+async function populate_action() {
+    let message = {
+        action: "EXECUTE",
+    }
+    function set_stack(items) {
+        console.log(items);
+        try{
+            delete items['stack']['stack'];
+        } catch(e) {
+            console.log(e)
+        } finally {
+            chrome.storage.local.set({ "stack": items })
+            .then(() => {
+                chrome.tabs.sendMessage(tabs[0].id, message);
             })
         }
-        chrome.storage.local.get(null, (items) => {
-            remove_status(items);
-            set_stack(items);
-        })
-
     }
+
+    function remove_status(items) {
+        const item_keys = Object.keys(items)
+        item_keys.forEach(weekday=>{
+            if(weekday != "stack") {
+                if(items[weekday].hasOwnProperty("status")) {
+                    delete items[weekday]["status"];
+                }
+            }
+        })
+    }
+    chrome.storage.local.get(null, (items) => {
+        remove_status(items);
+        set_stack(items);
+    })
+
+}
+    
     function initialize() {
         console.log('init')
         document.getElementById('populate').onclick = populate;
